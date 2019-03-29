d195e357bd85275a2de951219f2d74e1c0e552ca
kimkm0828 <kimkm0828@naver.com>
2019-03-29 12:19:00 +0900

    ml Test

---
+++ xor-train.py
@@ -0,0 +1,46 @@
+from sklearn import svm
+
+xor_data = [
+    #P,Q,result
+    [0,0,0],
+    [0,1,1],
+    [1,0,1],
+    [1,1,0]
+]
+
+# 문제와 답을 분리
+data = []
+label = []
+for row in xor_data:
+    p = row[0]
+    q = row[1]
+    r = row[2]
+    data.append([p,q])
+    label.append(r)
+
+# 데이터 학습시키기
+clf = svm.SVC()  # 객체생성
+clf.fit(data,label) # 학습
+
+
+# 데이터 예측하기
+pre = clf.predict(data)
+print("예측결과:",pre)
+
+
+# 결과 확인하기
+ok = 0
+total = len(label)
+for i in range(len(label)):
+    answer = label[i]
+    p = pre[i]
+    if answer == p:
+        ok += 1
+# ok = 0; total = 0
+# for idx, answer in enumerate(label):
+#     p = pre[idx]
+#     if p == answer:
+#         ok += 1
+#     total += 1
+print("정답률:",ok,"/",total,"=",ok/total)
+
